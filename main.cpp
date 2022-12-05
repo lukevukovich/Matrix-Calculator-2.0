9e076f33a113997d22c0770578cbca6af3ae9d32
lukevukovich <lukevuke83@gmail.com>
2022-12-04 21:10:20 -0500

    Added input error checking & comments

--- main.cpp
+++ main.cpp
@@ -2,75 +2,89 @@
 #include <cctype>
 #include <unistd.h>
 #include "Controller/Controller.h"
+#include "Calculator/Calculator.h"
 
 using namespace std;
+
+/*
+ * Matrix Calculator 2.0 so far adds:
+ * -OOP
+ * -Input error checking
+ * -Choice to keep previous matrix data
+ */
 
 int main() {
     //Keeps track of number of times program runs through while loop
     int programCount = 0;
 
+    //Operation and choice variables
     char o, c;
+
     bool quit = false, keepPrevData = false;
 
-    Controller controller = Controller();
+    //Calculator object that controls entire program
+    Calculator calculator = Calculator();
 
     while (!quit) {
 
         cout << ("\nMATRIX CALCULATOR:\n(+) Add\n(-) Subtract\n(*) Multiply\n(t) Transpose\n(x) Quit\n");
 
         //Get operation input
-        o = controller.getOperationInput();
-        controller.setOperation(o);
+        o = calculator.operationInput();
+        calculator.setOperation(o);
 
         //Only ask to keep previous data after first pass through
-        if (programCount > 0 && tolower(controller.getOperation()) != 'x') {
+        if (programCount > 0 && tolower(calculator.getOperation()) != 'x') {
             //Get input to keep previous data
-            c = controller.getChoiceInput();
-            controller.setChoice(c);
+            c = calculator.choiceInput();
+            calculator.setChoice(c);
 
-            if (controller.getChoice() == 'y')
+            //If yes, set boolean value
+            if (calculator.getChoice() == 'y')
                 keepPrevData = true;
             else
                 keepPrevData = false;
         }
 
+        //If user does not opt to keep prev data
         if (!keepPrevData) {
             //Set all matrix values to 0
-            controller.resetAll();
+            calculator.resetAll();
 
-            if (controller.getOperation() != 'x') {
+            if (calculator.getOperation() != 'x') {
                 //Perform first matrix data input
-                controller.operandInput(FIRST);
+                calculator.operandInput(FIRST);
 
-                if (controller.getOperation() != 't')
+                if (calculator.getOperation() != 't')
                     //Perform second matrix data input
-                    controller.operandInput(SECOND);
+                    calculator.operandInput(SECOND);
             }
         }
 
         //Perform corresponding operation
-        switch (controller.getOperation()) {
+        switch (calculator.getOperation()) {
             case '+':
-                controller.add();
+                calculator.add();
                 break;
             case '-':
-                controller.sub();
+                calculator.sub();
                 break;
             case '*':
-                controller.mult();
+                calculator.mult();
                 break;
             case 't':
             case 'T':
-                controller.trans();
+                calculator.trans();
                 break;
             case 'x':
             case 'X':
-                //Quit and break if x is entered
+                //Exit loop if x is entered
                 cout << ("\nQUIT CALCULATOR\n");
                 quit = true;
                 break;
         }
 
+        //Add to program counter
         programCount++;
 
     }
